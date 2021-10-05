b8a2ea74b6f69b51866603164902a018cd83325c
idiliev18 <idiliev18@codingburgas.bg>
2021-10-05 14:37:10 +0300

    Add project files.

---
+++ nakov-zadachi/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace nakov_zadachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
